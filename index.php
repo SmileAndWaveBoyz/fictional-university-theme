82fc68518af8dd9f765c570f7edf8502d98f84b4
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-10-27 10:58:03 +0000

    A Few Quick Edits / Improvements

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
     <div class="full-width-split__one">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-
+        
         <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
             <span class="event-summary__month">Mar</span>
-            <span class="event-summary__day">25</span>
+            <span class="event-summary__day">25</span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
@@ -28,14 +28,14 @@
         <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
             <span class="event-summary__month">Apr</span>
-            <span class="event-summary__day">02</span>
+            <span class="event-summary__day">02</span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
             <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
           </div>
         </div>
-
+        
         <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
 
       </div>
@@ -47,7 +47,7 @@
         <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="#">
             <span class="event-summary__month">Jan</span>
-            <span class="event-summary__day">20</span>
+            <span class="event-summary__day">20</span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
@@ -57,14 +57,14 @@
         <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="#">
             <span class="event-summary__month">Feb</span>
-            <span class="event-summary__day">04</span>
+            <span class="event-summary__day">04</span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
             <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
           </div>
         </div>
-
+        
         <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
     </div>
