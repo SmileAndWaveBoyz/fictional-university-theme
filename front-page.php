87110c4f4ca35f7ab7229ed642b4a8f9b6aa6392
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-11-02 16:50:24 +0000

    Quick Program Edits

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
       <h1 class="headline headline--large">Welcome!</h1>
       <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
       <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-      <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+      <a href="<?php echo get_post_type_archive_link('program') ?>" class="btn btn--large btn--blue">Find Your Major</a>
     </div>
   </div>
 
@@ -15,7 +15,7 @@
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-        <?php 
+        <?php
           $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
@@ -41,7 +41,7 @@
                   $eventDate = new DateTime(get_field('event_date'));
                   echo $eventDate->format('M')
                 ?></span>
-                <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
+                <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
@@ -54,7 +54,7 @@
             </div>
           <?php }
         ?>
-        
+
         <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
 
       </div>
@@ -72,7 +72,7 @@
             <div class="event-summary">
               <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
                 <span class="event-summary__month"><?php the_time('M'); ?></span>
-                <span class="event-summary__day"><?php the_time('d'); ?></span>  
+                <span class="event-summary__day"><?php the_time('d'); ?></span>
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
@@ -84,11 +84,11 @@
               </div>
             </div>
           <?php } wp_reset_postdata();
-        ?> 
+        ?>
 
-        
-        
-        
+
+
+
         <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
     </div>
--- single-program.php
+++ single-program.php
@@ -43,14 +43,19 @@
           )
         ));
 
-        while($homepageEvents->have_posts()) {
-          $homepageEvents->the_post(); ?>
+        if ($homepageEvents->have_posts()) {
+
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+          while($homepageEvents->have_posts()) {
+            $homepageEvents->the_post(); ?>
           <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month"><?php
                 $eventDate = new DateTime(get_field('event_date'));
                 echo $eventDate->format('M')
-              ?></span>
+                ?></span>
               <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
             </a>
             <div class="event-summary__content">
@@ -59,10 +64,11 @@
                   echo get_the_excerpt();
                 } else {
                   echo wp_trim_words(get_the_content(), 18);
-                  } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
             </div>
           </div>
-        <?php }
+          <?php }
+      }
       ?>
     </div>
 
