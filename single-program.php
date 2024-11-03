ed6bc6f4ef32a86fe244862ecbb7b32e398fda88
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-11-03 11:32:22 +0000

    Professors Post Type

--- single-program.php
+++ single-program.php
@@ -15,59 +15,91 @@
     </div>
 
     <div class="container container--narrow page-section">
-          <div class="metabox metabox--position-up metabox--with-home-link">
+      <div class="metabox metabox--position-up metabox--with-home-link">
         <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span></p>
       </div>
 
       <div class="generic-content"><?php the_content(); ?></div>
+
       <?php
-        $today = date('Ymd');
-        $homepageEvents = new WP_Query(array(
-          'posts_per_page' => 10,
-          'post_type' => 'event',
-          'meta_key' => 'event_date',
-          'orderby' => 'meta_value_num',
-          'order' => 'ASC',
-          'meta_query' => array(
-            array(
-              'key' => 'event_date',
-              'compare' => '>=',
-              'value' => $today,
-              'type' => 'numeric',
-            ),
-            array(
-              'key' => 'related_programs',
-              'compare' => 'LIKE',
-              'value' => '"' . get_the_ID() . '"',
-            )
+
+      $relatedProfessors = new WP_Query(array(
+        'posts_per_page' => -1,
+        'post_type' => 'professor',
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"',
           )
-        ));
+        )
+      ));
 
-        if ($homepageEvents->have_posts()) {
+      if ($relatedProfessors->have_posts()) {
 
-          echo '<hr class="section-break">';
-          echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
 
-          while($homepageEvents->have_posts()) {
-            $homepageEvents->the_post(); ?>
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month"><?php
-                $eventDate = new DateTime(get_field('event_date'));
-                echo $eventDate->format('M')
-                ?></span>
-              <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p><?php if (has_excerpt()) {
-                  echo get_the_excerpt();
-                } else {
-                  echo wp_trim_words(get_the_content(), 18);
-                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-            </div>
+        while($relatedProfessors->have_posts()) {
+          $relatedProfessors->the_post();
+          ?>
+          <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+        <?php
+        }
+      }
+
+      wp_reset_postdata(); // We need to call this or the get_the_ID() won't work in the next bit, because it's been hijacked by previous query. You will need to use this when ever you want to use more than one custom query on the same page. 
+
+      $today = date('Ymd');
+      $homepageEvents = new WP_Query(array(
+        'posts_per_page' => 10,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric',
+          ),
+          array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"',
+          )
+        )
+      ));
+
+      if ($homepageEvents->have_posts()) {
+
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+        while($homepageEvents->have_posts()) {
+          $homepageEvents->the_post(); ?>
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month"><?php
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+              ?></span>
+            <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php if (has_excerpt()) {
+                echo get_the_excerpt();
+              } else {
+                echo wp_trim_words(get_the_content(), 18);
+              } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
           </div>
-          <?php }
+        </div>
+        <?php
+        }
       }
       ?>
     </div>
