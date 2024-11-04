0ecb939ceb5c2252efc6590304046402b3361720
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-11-04 18:44:29 +0000

    Quick Fix For Page Banner Function

--- single-event.php
+++ single-event.php
@@ -1,5 +1,5 @@
 <?php
-
+  
   get_header();
 
   while(have_posts()) {
@@ -11,7 +11,7 @@
         <div class="page-banner__intro">
           <p>DONT FORGET TO REPLACE ME LATER</p>
         </div>
-      </div>
+      </div>  
     </div>
 
     <div class="container container--narrow page-section">
@@ -27,26 +27,20 @@
 
         if ($relatedPrograms) {
           echo '<hr class="section-break">';
-          echo '<h2 class="headline headline--medium">Related Programs</h2>';
+          echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
           echo '<ul class="link-list min-list">';
-
-          foreach ($relatedPrograms as $program) {
-            ?>
-              <li>
-                <a href="<?php echo get_the_permalink($program)?>"><?php echo get_the_title($program); ?></a>
-              </li>
-            <?php
-          }
+          foreach($relatedPrograms as $program) { ?>
+            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+          <?php }
           echo '</ul>';
         }
-
 
       ?>
 
     </div>
+    
 
-
-
+    
   <?php }
 
   get_footer();
