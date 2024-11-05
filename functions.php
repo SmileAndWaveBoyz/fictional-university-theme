b62952e9a52d885d22868725231e68166db4bfb1
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-11-05 16:56:49 +0000

    Campus Post Type

--- functions.php
+++ functions.php
@@ -74,3 +74,10 @@ function university_adjust_queries($query) {
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+// function universityMapKey($api){
+//   $api['key'] = 'MyBalls';
+//   return $api;
+// }
+
+// add_filter('acf/fields/google_map/api', 'universityMapKey');
