87179c202438e8eb71ca761d90f7446831dd32a0
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-11-07 14:07:33 +0000

    Live Search

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'b93ea068eede415dd4b0');
+<?php return array('dependencies' => array(), 'version' => '7aaf4db41312c80c3f14');
---
+++ src/modules/Search.js
@@ -0,0 +1,7 @@
+class Search{
+    constructor(){
+        alert('Eh?')
+    }
+}
+
+export default Search
