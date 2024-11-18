1dce6b0465181b5eb29091ab1a284b2d766bf7fc
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-11-18 18:30:14 +0000

    Note Permissions and Security (Part 2)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'cae63ecd74d263885e17');
+<?php return array('dependencies' => array('jquery'), 'version' => 'ab1b98cef1691e918c9d');
--- src/modules/MyNotes.js
+++ src/modules/MyNotes.js
@@ -17,7 +17,7 @@ class MyNotes {
         var ourNewPost = {
             'title': $('.new-note-title').val(),
             'content': $('.new-note-body').val(),
-            'status': 'publish',
+            'status': 'private',
         }
 
         $.ajax({
