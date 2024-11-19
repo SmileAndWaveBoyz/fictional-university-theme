3864503ce67036d3d26da5119feaa52473c820a0
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2024-11-19 13:26:11 +0000

    Let Users "Like" Content (Part 1)

--- single-professor.php
+++ single-professor.php
@@ -1,15 +1,15 @@
 <?php
-  
+
   get_header();
 
   while(have_posts()) {
     the_post();
     pageBanner();
      ?>
-    
+
 
     <div class="container container--narrow page-section">
-          
+
       <div class="generic-content">
         <div class="row group">
 
@@ -18,6 +18,43 @@
           </div>
 
           <div class="two-thirds">
+            <?php
+              $likeCount = new WP_Query([
+                'post_type' => 'like',
+                'meta_query' => [
+                  [
+                    'key' => 'liked_professor_id',
+                    'compare' => '=',
+                    'value' => get_the_ID(),
+                  ]
+                ],
+              ]);
+
+              $existStatus = 'no';
+
+              $existQuery = new WP_Query([
+                'author' => get_current_user_id(),
+                'post_type' => 'like',
+                'meta_query' => [
+                  [
+                    'key' => 'liked_professor_id',
+                    'compare' => '=',
+                    'value' => get_the_ID(),
+                  ]
+                ],
+              ]);
+
+              if ($existQuery->found_posts) {
+                $existStatus = 'yes';
+              }
+
+            ?>
+
+            <span class="like-box" data-exists='<?php echo $existStatus?>'>
+              <i class="fa fa-heart-o" aria-hidden="true"></i>
+              <i class="fa fa-heart" aria-hidden="true"></i>
+              <span class="like-count"><?php echo $likeCount->found_posts?></span>
+            </span>
             <?php the_content(); ?>
           </div>
 
@@ -41,7 +78,7 @@
       ?>
 
     </div>
-    
+
   <?php }
 
   get_footer();
